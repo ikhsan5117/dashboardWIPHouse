838df2d0364904e6f81f85507efc7a01c76bb635
ikhsan5117 <ikhsanmuhammad37956@gemail.com>
2026-03-02 10:30:10 +0700

    update

--- obj/Debug/net8.0/dashboardWIPHouse.AssemblyInfo.cs
+++ obj/Debug/net8.0/dashboardWIPHouse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dashboardWIPHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+270a907cdc60753de2d9d2349cab826e46cb99c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2022223c54a85177dcac601eba34388c6f4095")]
 [assembly: System.Reflection.AssemblyProductAttribute("dashboardWIPHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dashboardWIPHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
